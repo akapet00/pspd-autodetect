6e66b2af87aa58bfb6a4b5a47133e58cb7882230
akapet00 <akapet00@gmail.com>
2023-06-02 10:47:32 +0200

    add option for bounding box

--- playground/figure_point_cloud.py
+++ playground/figure_point_cloud.py
@@ -5,6 +5,7 @@ import numpy as np
 
 
 elev, azim = 25, 50
+bounding_box = True
 
 # data
 fname = os.path.join('input', 'data', 'head.scaled')
@@ -13,10 +14,10 @@ N = points.shape[0]
 num = 0.1 * N
 mask = np.arange(0, N, int(N/num))
 x, y, z = points[mask].T
-xmin, ymin, zmin = np.min(points[mask], axis=0)
-xmax, ymax, zmax = np.max(points[mask], axis=0)
 
 # bounding box
+xmin, ymin, zmin = np.min(points[mask], axis=0)
+xmax, ymax, zmax = np.max(points[mask], axis=0)
 A = [xmax, ymin, zmin]
 B = [xmax, ymax, zmin]
 C = [xmin, ymax, zmin]
@@ -31,18 +32,19 @@ fig = plt.figure(figsize=(5, 5))
 ax = plt.axes(projection='3d')
 ax.scatter(x, y, z,
            fc='w', ec='k', s=5, lw=0.5, rasterized=True)
-ax.plot(*np.c_[A, B], c='k', lw=1)
-ax.plot(*np.c_[A, D], c='k', lw=1)
-ax.plot(*np.c_[A, E], c='k', lw=1)
-ax.plot(*np.c_[B, C], c='k', lw=1)
-ax.plot(*np.c_[B, F], c='k', lw=1, zorder=3)
-ax.plot(*np.c_[C, D], c='k', lw=1)
-ax.plot(*np.c_[C, G], c='k', lw=1)
-ax.plot(*np.c_[D, H], c='k', lw=1)
-ax.plot(*np.c_[E, F], c='k', lw=1, zorder=3)
-ax.plot(*np.c_[E, H], c='k', lw=1)
-ax.plot(*np.c_[F, G], c='k', lw=1, zorder=3)
-ax.plot(*np.c_[G, H], c='k', lw=1)
+if bounding_box:
+    ax.plot(*np.c_[A, B], c='k', lw=1)
+    ax.plot(*np.c_[A, D], c='k', lw=1)
+    ax.plot(*np.c_[A, E], c='k', lw=1)
+    ax.plot(*np.c_[B, C], c='k', lw=1)
+    ax.plot(*np.c_[B, F], c='k', lw=1, zorder=3)
+    ax.plot(*np.c_[C, D], c='k', lw=1)
+    ax.plot(*np.c_[C, G], c='k', lw=1)
+    ax.plot(*np.c_[D, H], c='k', lw=1)
+    ax.plot(*np.c_[E, F], c='k', lw=1, zorder=3)
+    ax.plot(*np.c_[E, H], c='k', lw=1)
+    ax.plot(*np.c_[F, G], c='k', lw=1, zorder=3)
+    ax.plot(*np.c_[G, H], c='k', lw=1)
 ax.set_box_aspect(np.ptp(points, axis=0))
 ax.set_axis_off()
 ax.view_init(elev, azim)
--- playground/figure_surface.py
+++ playground/figure_surface.py
@@ -7,6 +7,7 @@ import open3d as o3d
 
 
 elev, azim = 25, 50
+bounding_box = False
 
 # data
 fname = os.path.join('input', 'data', 'head.scaled')
@@ -14,16 +15,42 @@ points = np.loadtxt(fname + '.xyz')
 normals = np.loadtxt(fname + '.normals')
 mesh = o3d.io.read_triangle_mesh(fname + '.iso.watertight.off')
 
+# bounding box
+xmin, ymin, zmin = np.min(points, axis=0)
+xmax, ymax, zmax = np.max(points, axis=0)
+A = [xmax, ymin, zmin]
+B = [xmax, ymax, zmin]
+C = [xmin, ymax, zmin]
+D = [xmin, ymin, zmin]
+E = [xmax, ymin, zmax]
+F = [xmax, ymax, zmax]
+G = [xmin, ymax, zmax]
+H = [xmin, ymin, zmax]
+
 # surface
 mesh_smp = mesh.simplify_quadric_decimation(6500)
 vert_smp = np.asarray(mesh_smp.vertices)
 tri_smp = np.asarray(mesh_smp.triangles)
 ls = LightSource(elev+20, azim)
 
+# surface in a bounding box
 fig = plt.figure(figsize=(5, 5))
 ax = plt.axes(projection='3d')
 ax.plot_trisurf(*vert_smp.T, triangles=tri_smp,
                 color='gray', ec='k', lw=0.05, lightsource=ls)
+if bounding_box:
+    ax.plot(*np.c_[A, B], c='k', lw=1)
+    ax.plot(*np.c_[A, D], c='k', lw=1)
+    ax.plot(*np.c_[A, E], c='k', lw=1)
+    ax.plot(*np.c_[B, C], c='k', lw=1)
+    ax.plot(*np.c_[B, F], c='k', lw=1, zorder=3)
+    ax.plot(*np.c_[C, D], c='k', lw=1)
+    ax.plot(*np.c_[C, G], c='k', lw=1)
+    ax.plot(*np.c_[D, H], c='k', lw=1)
+    ax.plot(*np.c_[E, F], c='k', lw=1, zorder=3)
+    ax.plot(*np.c_[E, H], c='k', lw=1)
+    ax.plot(*np.c_[F, G], c='k', lw=1, zorder=3)
+    ax.plot(*np.c_[G, H], c='k', lw=1)
 ax.set_box_aspect(np.ptp(points, axis=0))
 ax.set_axis_off()
 ax.view_init(elev, azim)
