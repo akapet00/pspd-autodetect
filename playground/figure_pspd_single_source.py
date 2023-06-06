7bcae3dfd45572fd0b73fc8c8a84955132fa1bbb
akapet00 <akapet00@gmail.com>
2023-06-06 10:53:46 +0200

    interpolate averaging area

--- playground/figure_pspd_single_source.py
+++ playground/figure_pspd_single_source.py
@@ -1,11 +1,12 @@
 import os
 
 import matplotlib.pyplot as plt
-
+from matplotlib.colors import LightSource
 from matplotlib.patches import Rectangle
 from mpl_toolkits.mplot3d.art3d import pathpatch_2d_to_3d
 import numpy as np
 import pickle
+from scipy.interpolate import CloughTocher2DInterpolator
 import seaborn as sns
 sns.set(style='white', font_scale=1.25,
         rc={'text.usetex' : True, 'font.family': 'serif'})
@@ -42,6 +43,14 @@ d = z_at_ymin - zmin
 r = np.sqrt(np.sqrt(area))
 angle_rad = 2 * np.arcsin(d / (2 * r))
 angle = np.rad2deg(angle_rad)
+
+# interpolated surface within square patch bounds
+Y, Z = np.mgrid[nbh[:, 1].min():nbh[:, 1].max():103j,
+                nbh[:, 2].min():nbh[:, 2].max():103j]
+Y = Y[1:-1, 1:-1]
+Z = Z[1:-1, 1:-1]
+xfun = CloughTocher2DInterpolator(nbh[:, 1:], nbh[:, 0])
+X = xfun(Y, Z)
 
 # search space
 fig = plt.figure(figsize=(5, 5))
@@ -104,5 +113,52 @@ ax.view_init(5, -15)
 formats = ['png', 'pdf']
 for ext in formats:
     fig.savefig(os.path.join('figures', f'pspd_single_source_2.{ext}'),
+                dpi=300,
+                pad_inches=None)
+
+# hot-spot region interpolated, zoom in
+elev = 5
+azim = -30
+ls = LightSource(15, -50)
+fig = plt.figure(figsize=(5, 5))
+ax = plt.axes(projection='3d')
+surf = ax.plot_surface(X, Y, Z,
+                       rcount=50,
+                       ccount=50,
+                       color='lightgray',
+                       shade=True,
+                       lightsource=ls,
+                       linewidth=0,
+                       antialiased=False,
+                       zorder=1)
+ax.plot(*p, 'o', ms=7, mfc='r', mec='k', mew=1.5, zorder=2)
+square = Rectangle(xy=(ymin, z_at_ymin - 0.15 * z_at_ymin),
+                   width=np.sqrt(area),
+                   height=np.sqrt(area),
+                   angle=-angle,
+                   ec='k', fc='none', lw=2, zorder=3)
+ax.add_patch(square)
+pathpatch_2d_to_3d(square, z=nbh[:, 0].max(), zdir='x')
+ax.set_box_aspect(np.ptp(nbh, axis=0))
+ax.set(xlabel='',
+       ylabel='',
+       zlabel='',
+       xticks=[],
+       yticks=[-4.4, -3.2, -2.0],
+       zticks=[-3.1, -1.8, -.5],
+       yticklabels=['', '', ''],
+       zticklabels=['', '', ''])
+ax.xaxis.pane.fill = False
+ax.yaxis.pane.fill = False
+ax.zaxis.pane.fill = False
+ax.xaxis.pane.set_edgecolor('w')
+ax.yaxis.pane.set_edgecolor('w')
+ax.zaxis.pane.set_edgecolor('w')
+ax.view_init(elev, azim)
+plt.show()
+
+formats = ['png', 'pdf']
+for ext in formats:
+    fig.savefig(os.path.join('figures', f'pspd_single_source_3.{ext}'),
                 dpi=300,
                 pad_inches=None)
